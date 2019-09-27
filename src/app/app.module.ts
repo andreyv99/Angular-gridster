cde85964f2fac3759b8b9ba65f799d6647473343
Andrew Voronov <avoronov@madisonpg.com>
2019-09-27 19:54:52 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { GridsterModule } from 'angular-gridster2';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { Tile1Component } from './components/tile1/tile1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    Tile1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/tile1/tile1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gridster',
+  templateUrl: './tile1.component.html',
+  styleUrls: ['./tile1.component.scss']
+})
+export class Tile1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+ 
